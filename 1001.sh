9f434cc4c2f47a7b616abbd10a527855d7ea97af
Singer-id <2586611985@qq.com>
2022-10-04 20:14:13 +0800

    add 2_1 sh

--- 1001.sh
+++ 1001.sh
@@ -36,4 +36,4 @@ python T5.py --model_checkpoint save/t5-smallt5_except_domain_taxi_lr_0.0001_epo
 python T5.py --model_checkpoint save/t5-smallt5_except_domain_taxi_lr_0.0001_epoch_1_seed_558_batch_size_4_auxiliary_task_0.25_base_False_task2first_True/ --n_epochs 5 --train_batch_size 4 --seed 558 --auxiliary_task_ratio 0.25 --fewshot 0.1 --mode finetune --task2first
 
 
-python T5.py --n_epochs 1 --train_batch_size 4 --seed 558 --auxiliary_task_ratio 1.0 --task2first
+python T5.py --n_epochs 1 --train_batch_size 4 --seed 558 --model_checkpoint save/t5-smallt5_except_domain_none_lr_0.0001_epoch_1_seed_558_batch_size_4_auxiliary_task_1.0_base_False_task2first_True/ --auxiliary_task_ratio 1.0 --task2first
